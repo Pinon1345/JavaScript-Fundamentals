99edcd307e1c3e824b02c27d830d07e8d0302da5
PINON <pinonfurkan1@gmail.com>
2026-09-03 16:06:44 +0600

    values in JS

---
+++ values.js
@@ -0,0 +1,12 @@
+// falsy value: false, 0, "", undefined, NaN, null;
+let age = 1;
+
+if (age) {
+  console.log("Age is exist");
+}
+else{
+    console.log("Nothing is here");
+}
+
+
+console.log(typeof NaN);
